cb11279e04d744a1389917c026c00e3475f39bf5
OSPK <waqas@opensource.com.pk>
2015-02-12 22:36:14 +0500

    add json view file

---
+++ view.php
@@ -0,0 +1,28 @@
+<?php
+$server = $_SERVER['HTTP_HOST'];
+
+$url = "http://". $server . "/matches.json";
+$json = file_get_contents($url);				
+$obj = json_decode($json);
+echo "<h1>MATCHES</h1>";
+echo "<br>======================================================================================<br><pre>";
+print_r($obj);
+echo "<h1>## END MATCHES ##</h1>";
+echo "<br>======================================================================================<br><pre>";
+
+$numbers = [0,1,2,3,4];
+
+foreach ($numbers as $number) {
+
+	$url = "http://". $server . "/$number.json";
+	$json = file_get_contents($url);				
+	$obj = json_decode($json);
+	echo "<h1>$number</h1>";
+	echo "<br>======================================================================================<br><pre>";
+	print_r($obj);
+	echo "<h1>## END $number ##</h1>";
+	echo "<br>======================================================================================<br><pre>";
+
+
+} 
+?>
