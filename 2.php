fe8958b225e35a72cb29114c97c057f33e67d533
OSPK <waqas@opensource.com.pk>
2015-02-12 21:04:00 +0500

    date format

--- 2.php
+++ 2.php
@@ -22,9 +22,12 @@ include_once "2functions.php";
 	<body>
 		<section>
 			<?php 
+			echo get_current_user();
+			
 			if (!isset($_GET['match'])) {
 				$url = "https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20cricket.upcoming_matches%20WHERE%20series_id%3D%2211737%22&format=json&env=store%3A%2F%2F0TxIGQMQbObzvU4Apia0V0&callback=";
 				$json = file_get_contents($url);
+
 				$obj = json_decode($json);
 				$matches = $obj->query->results->Match;
 				//$obj = array_slice($obj, 0,4);
