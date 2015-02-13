274ba2fe3d71f4370b8f313d9fffac941ec1656e
OSPK <waqas@opensource.com.pk>
2015-02-14 03:32:55 +0500

    faster robot

--- robot.php
+++ robot.php
@@ -2,7 +2,7 @@
 $server = $_SERVER['HTTP_HOST'];
 if (isset($_GET['write'])) {$this_write='write=' . $_GET['write'];} else {$this_write=null;}
 ?>
-<meta http-equiv="refresh" content="15; url=http://<?php echo $server . '/robot.php?' . $this_write; ?>" />
+<meta http-equiv="refresh" content="10; url=http://<?php echo $server . '/robot.php?' . $this_write; ?>" />
 <?php
 if (isset($_GET['write'])=='go') {
 
