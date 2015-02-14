361b1f7d1b904bebe566c4eabda2528d1c9b5721
OSPK <waqas@opensource.com.pk>
2015-02-14 17:32:15 +0500

    CLEANUP

--- widcontent.php
+++ widcontent.php
@@ -1,13 +1,13 @@
 <?php 
-//$server = $_SERVER['HTTP_HOST'];
+$server = $_SERVER['HTTP_HOST'];
 
-//$url = "http://". $server . "/data/live.json";
-//$json = file_get_contents($url);
-//$obj = json_decode($json);
+$url = "http://". $server . "/data/live.json";
+$json = file_get_contents($url);
+$obj = json_decode($json);
 
-//if ($obj->query->count > 0) {
-//	$scores = $obj->query->results->Scorecard;
-//}
+if ($obj->query->count > 0) {
+	$scores = $obj->query->results->Scorecard;
+}
 
 ?>
 	<?php 
