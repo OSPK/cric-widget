d3947e3d1a8e62a2a99e30f3fcf13c01a68959ab
OSPK <waqas@opensource.com.pk>
2015-02-16 23:24:59 +0500

    ads

--- detail.php
+++ detail.php
@@ -34,6 +34,11 @@
 				include_once "detailcontent.php";
 			?>
 		</section>
+		<div class="adlarge">
+			<!--a href="http://dailypakistan.com.pk/cricket-world-cup-2015/15-Feb-2015/194394" target="_parent">
+			PAK vs India: Live Scorecard
+			</a-->
+		</div>
 
 		<!-- jQuery -->
 		<script src="//code.jquery.com/jquery.js"></script>
