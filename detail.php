7d1637a28484fe3315f78b9e5320eb9fd2f2f18c
OSPK <waqas@opensource.com.pk>
2015-02-18 11:13:52 +0500

    ad

--- detail.php
+++ detail.php
@@ -37,7 +37,7 @@
 		<div class="adlarge">
 			<!--a href="http://dailypakistan.com.pk/cricket-world-cup-2015/15-Feb-2015/194394" target="_parent">
 			PAK vs India: Live Scorecard
-			</a-->
+			</a-->&nbsp;
 		</div>
 
 		<!-- jQuery -->
