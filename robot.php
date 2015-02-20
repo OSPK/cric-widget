66b0981dc6420baea2bc64c9488e3bd5869e174a
OSPK <waqas@opensource.com.pk>
2015-02-20 22:14:14 +0500

    up

--- robot.php
+++ robot.php
@@ -4,7 +4,7 @@ if (isset($_GET['write'])) {$this_write='write=' . $_GET['write'];} else {$this_
 ?>
 <meta http-equiv="refresh" content="10; url=http://<?php echo $server . '/robot.php?' . $this_write; ?>" />
 <?php
-if (isset($_GET['write'])=='goddamit') {
+if (isset($_GET['write'])=='go') {
 
 	$url = "https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20cricket.upcoming_matches%20WHERE%20series_id%3D%2211737%22&format=json&env=store%3A%2F%2F0TxIGQMQbObzvU4Apia0V0&callback=";
 	$json = file_get_contents($url);
