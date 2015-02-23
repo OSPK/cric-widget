8929689d5d0636cb2215df5731261310f6ae22aa
OSPK <waqas@opensource.com.pk>
2015-02-23 16:11:44 +0500

    points table

---
+++ points.php
@@ -0,0 +1,332 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Cric Widget</title>
+		<!--CSS -->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
+		<link href="assets/style.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="pointsheader"><strong>World Cup Points Table</strong></div>
+		<section class="pointswidget">
+			<div class="table-responsive">
+			    <table class="table">
+			        <thead style="margin-bottom:10px;">
+			            <tr>
+			                <th>Pool A</th>
+
+			                <th>P</th>
+
+			                <th>W</th>
+
+			                <th>L</th>
+
+			                <th>Tied</th>
+
+			                <th>NR</th>
+
+			                <th>Pts</th>
+
+			                <th>Nrr</th>
+			            </tr>
+			        </thead>
+
+			        <tbody>
+			            <tr class="cb-column-highlighter">
+			                <td class="team_name">New Zealand</td>
+
+			                <td>3</td>
+
+			                <td>3</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>6</td>
+
+			                <td>+3.59</td>
+			            </tr>
+
+			            <tr class="cb-column-highlighter">
+			                <td class="team_name">Australia</td>
+
+			                <td>2</td>
+
+			                <td>1</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>1</td>
+
+			                <td>3</td>
+
+			                <td>+2.22</td>
+			            </tr>
+
+			            <tr class="cb-column-highlighter">
+			                <td class="team_name">Bangladesh</td>
+
+			                <td>2</td>
+
+			                <td>1</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>1</td>
+
+			                <td>3</td>
+
+			                <td>+2.10</td>
+			            </tr>
+
+			            <tr class="cb-column-highlighter">
+			                <td class="team_name">Sri Lanka</td>
+
+			                <td>2</td>
+
+			                <td>1</td>
+
+			                <td>1</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>2</td>
+
+			                <td>-0.86</td>
+			            </tr>
+
+			            <tr class="">
+			                <td class="team_name">England</td>
+
+			                <td>3</td>
+
+			                <td>1</td>
+
+			                <td>2</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>2</td>
+
+			                <td>-1.42</td>
+			            </tr>
+
+			            <tr class="">
+			                <td class="team_name">Afghanistan</td>
+
+			                <td>2</td>
+
+			                <td>0</td>
+
+			                <td>2</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>-1.18</td>
+			            </tr>
+
+			            <tr class="">
+			                <td class="team_name">Scotland</td>
+
+			                <td>2</td>
+
+			                <td>0</td>
+
+			                <td>2</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>0</td>
+
+			                <td>-2.74</td>
+			            </tr>
+			        </tbody>
+			    </table>
+			</div>
+			<br><br>
+				<div class="table-responsive">
+				    <table class="table">
+				        <thead style="margin-bottom:10px;">
+				            <tr>
+				                <th>Pool B</th>
+
+				                <th>P</th>
+
+				                <th>W</th>
+
+				                <th>L</th>
+
+				                <th>Tied</th>
+
+				                <th>NR</th>
+
+				                <th>Pts</th>
+
+				                <th>Nrr</th>
+				            </tr>
+				        </thead>
+
+				        <tbody>
+				            <tr class="cb-column-highlighter">
+				                <td class="team_name">India</td>
+
+				                <td>2</td>
+
+				                <td>2</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>4</td>
+
+				                <td>+2.06</td>
+				            </tr>
+
+				            <tr class="cb-column-highlighter">
+				                <td class="team_name">West Indies</td>
+
+				                <td>2</td>
+
+				                <td>1</td>
+
+				                <td>1</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>2</td>
+
+				                <td>+1.27</td>
+				            </tr>
+
+				            <tr class="cb-column-highlighter">
+				                <td class="team_name">Ireland</td>
+
+				                <td>1</td>
+
+				                <td>1</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>2</td>
+
+				                <td>+0.62</td>
+				            </tr>
+
+				            <tr class="cb-column-highlighter">
+				                <td class="team_name">Zimbabwe</td>
+
+				                <td>2</td>
+
+				                <td>1</td>
+
+				                <td>1</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>2</td>
+
+				                <td>-0.50</td>
+				            </tr>
+
+				            <tr class="">
+				                <td class="team_name">South Africa</td>
+
+				                <td>2</td>
+
+				                <td>1</td>
+
+				                <td>1</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>2</td>
+
+				                <td>-0.68</td>
+				            </tr>
+
+				            <tr class="">
+				                <td class="team_name">UAE</td>
+
+				                <td>1</td>
+
+				                <td>0</td>
+
+				                <td>1</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>-0.26</td>
+				            </tr>
+
+				            <tr class="">
+				                <td class="team_name">Pakistan</td>
+
+				                <td>2</td>
+
+				                <td>0</td>
+
+				                <td>2</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>0</td>
+
+				                <td>-2.26</td>
+				            </tr>
+				        </tbody>
+				    </table>
+				</div>
+		</section>
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+	</body>
+</html>
