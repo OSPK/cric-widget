31d6caee444dd5e1c89652e32ec7a4338006e39c
OSPK <waqas@opensource.com.pk>
2015-03-25 17:40:42 +0500

    mobilink test

--- block.php
+++ block.php
@@ -1,6 +1,8 @@
 <?php
+
+echo "Your IP:" . $_SERVER['REMOTE_ADDR']."<br>";
 if ($_SERVER['REMOTE_ADDR']=='104.236.65.135') {
-	echo "This code appears if you are not Mobilink";
+	echo "This code appears if you are Mobilink";
 }
 
 else {
