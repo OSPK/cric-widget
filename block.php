3b506730d5c319a0c18ee76177c853b87fdd8e30
OSPK <waqas@opensource.com.pk>
2015-03-25 18:05:22 +0500

    mobilink test

--- block.php
+++ block.php
@@ -1,12 +1,37 @@
 <?php
 
-echo "Your IP:" . $_SERVER['REMOTE_ADDR']."<br>";
-if ($_SERVER['REMOTE_ADDR']=='104.236.65.135') {
-	echo "This code appears if you are Mobilink";
+/**
+ * Check if a given ip is in a network
+ * @param  string $ip    IP to check in IPV4 format eg. 127.0.0.1
+ * @param  string $range IP/CIDR netmask eg. 127.0.0.0/24, also 127.0.0.1 is accepted and /32 assumed
+ * @return boolean true if the ip is in this range / false if not.
+ */
+function ip_in_range( $ip, $range ) {
+	if ( strpos( $range, '/' ) == false ) {
+		$range .= '/32';
+	}
+	// $range is in IP/CIDR format eg 127.0.0.1/24
+	list( $range, $netmask ) = explode( '/', $range, 2 );
+	$range_decimal = ip2long( $range );
+	$ip_decimal = ip2long( $ip );
+	$wildcard_decimal = pow( 2, ( 32 - $netmask ) ) - 1;
+	$netmask_decimal = ~ $wildcard_decimal;
+	return ( ( $ip_decimal & $netmask_decimal ) == ( $range_decimal & $netmask_decimal ) );
+}
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$range1 = '119.160.116.0/22';
+$range2 = '119.160.64.0/20';
+
+echo "Your IP:" . $ip ."<br><br>";
+
+
+if ( ip_in_range( $ip, $range1 ) || ip_in_range( $ip, $range2 ) ) {
+	echo "You are Mobilink";
 }
 
 else {
-	echo "This code appears if you are Others";
+	echo "You are NOT Mobilink";
 }
 
 ?>
